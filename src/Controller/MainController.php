345ef2b6cac6cfcddcd9dd3a8bd1cf243e2043d5
juliusknows <ageev_m@icloud.com>
2026-01-26 16:50:09 +0300

    Сhanged Mainontroller
    - Проверил работу подключенных ранее твиг бандлов.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 final class MainController
 {
-    /*
-     * Главная страница
-     *
-     * @return Response
-     */
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
     public function index(): Response
     {
-        return new Response('Hello World');
+        $content = $this->twig->render('base.html.twig');
+        return new Response($content);
     }
 }
